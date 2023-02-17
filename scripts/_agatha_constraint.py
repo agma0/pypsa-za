d9f5102fb7366477ad41ca8ff744579792f9ae92
agma0 <agatha.majcher@tu-berlinde>
2023-02-17 12:17:55 +0100

    added changes #2 2030

--- scripts/_agatha_constraint.py
+++ scripts/_agatha_constraint.py
@@ -27,24 +27,27 @@ from pypsa.linopf import (
 # ZAR 1,4 billion in 2021 with ZAR 122/tCO2e -> hochskalieren! Prognose 2030?
 max_add_carbon_investment = 1400000000
 
-def add_carbontax_contraints(n):
+def add_carbontax_contraints(n, year=2030):
     renewable_carriers = ['solar', 'onwind', 'CSP', 'biomass', 'hydro'] # in config.yaml deklariert #hydroimport???
-    add_generators = n.generators[n.generators['carrier'].isin(renewable_carriers)]
-    add_storage_units = n.storage_units[n.storage_units['carrier'] == 'PHS']
+    add_generators = n.generators[(n.generators['carrier'].isin(renewable_carriers))
+                                  & (n.generators.build_year==year)]
+    add_storage_units = n.storage_units[(n.storage_units['carrier'] == 'PHS')
+                                        & (n.storage_units.build_year==year)]
 
     if add_generators.empty and add_storage_units.empty or ('Generator', 'p_nom') not in n.variables.index:
         return
 
     generators_p_nom = get_var(n, "Generator", "p_nom")
 
+    stores_p_nom = get_var(n, "StorageUnit", "p_nom")
     lhs = linexpr((add_generators['capital_cost'], generators_p_nom[add_generators.index])).sum()
-    lhs += linexpr((add_storage_units['capital_cost'],)).sum()
+    lhs += linexpr((add_storage_units['capital_cost'],stores_p_nom[add_storage_units.index])).sum()
 
-    define_constraints(n, lhs, "<=", max_add_carbon_investment, 'Generator&Storage', 'additional_carbontax_investment')
+    define_constraints(n, lhs, "<=", max_add_carbon_investment, 'Generator-Storage', 'additional_carbontax_investment')
 
 ### ideas
 # define 25% , 50%, 75%, 100% of revenues - show scenarios
-# climate goals 2030 - 400 MtCO2e ? -> change CO2 limit in config.yaml line 69
+# climate goals 2030 - 400 MtCO2e ? -> energy/electricity share - change CO2 limit in config.yaml line 69
 # end load shedding? changes in loadshedding (2030) if revenues are invested in RE?
 
 # add new data SACAD/SAPAD? 2022?
