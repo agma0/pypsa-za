a151b74f80d4bef3c6779ae81ea67a2076717bc6
agma0 <agatha.majcher@tu-berlinde>
2023-02-17 12:10:00 +0100

    test script to check pyomo and solver

---
+++ scripts/_test_pyomo_glpk.py
@@ -0,0 +1,18 @@
+import pyomo.environ as pyo
+
+model = pyo.ConcreteModel()
+
+model.x = pyo.Var(within=pyo.NonNegativeReals)
+model.y = pyo.Var(within=pyo.NonNegativeReals)
+
+model.obj = pyo.Objective(expr=2*model.x + 3*model.y)
+
+model.con1 = pyo.Constraint(expr=3*model.x + 4*model.y >= 1)
+model.con2 = pyo.Constraint(expr=2*model.x + model.y >= 1)
+
+solver = pyo.SolverFactory('glpk')
+
+solver.solve(model)
+
+print('x = ', pyo.value(model.x))
+print('y = ', pyo.value(model.y))
