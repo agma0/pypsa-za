b834442ea7ad49467c6daf30a65901fe6949110f
agma0 <93915754+agma0@users.noreply.github.com>
2023-03-25 20:11:00 +0100

    latest code of carbon tax constraint

--- scripts/_agatha_constraint.py
+++ scripts/_agatha_constraint.py
@@ -24,11 +24,14 @@ from pypsa.linopf import (
 #     StorageUnit: [battery, PHS]
 
 
-# ZAR 8 billion in 2030 -> Prognosis 2030 Worldbank (2022)
-max_add_carbon_investment = 8e9
+# ZAR 1,4 billion in 2021 with ZAR 122/tCO2e ->1,4e9
+# 8 billion USD 8e9 bis 2030 -> avergae exchange 2022 - 17.673 -> 141.384 billion ZAR
+# 141.384 1e9 ZAR
+
+max_add_carbon_investment = 141.384e9
 
 def add_carbontax_contraints(n, year=2030):
-    renewable_carriers = ['solar', 'onwind', 'CSP', 'biomass', 'hydro'] # in config.yaml deklariert #hydroimport???
+    renewable_carriers = ['onwind', 'solar', 'CSP', 'biomass', 'hydro'] # in config.yaml deklariert #hydroimport???
     add_generators = n.generators[(n.generators['carrier'].isin(renewable_carriers))
                                   & (n.generators.build_year==year)]
     add_storage_units = n.storage_units[(n.storage_units['carrier'] == 'PHS')
@@ -43,11 +46,11 @@ def add_carbontax_contraints(n, year=2030):
     lhs = linexpr((add_generators['capital_cost'], generators_p_nom[add_generators.index])).sum()
     lhs += linexpr((add_storage_units['capital_cost'],stores_p_nom[add_storage_units.index])).sum()
 
-    define_constraints(n, lhs, "<=", max_add_carbon_investment, 'Generator-Storage', 'additional_carbontax_investment')
+    define_constraints(n, lhs, ">=", max_add_carbon_investment, 'Generator-Storage', 'additional_carbontax_investment')
 
 ### ideas
 # define 25% , 50%, 75%, 100% of revenues - show scenarios
-# climate goals 2030 - 400 MtCO2e ? -> energy/electricity share - change CO2 limit in config.yaml line 69
+# climate goals 2030 - 400 MtCO2e ? -> change CO2 limit in config.yaml line 69
 # end load shedding? changes in loadshedding (2030) if revenues are invested in RE?
 
 # add new data SACAD/SAPAD? 2022?
